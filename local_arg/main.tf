dc1203be4cf134650b3b75d469c31e9101bb9f51
Parag Kalra <paragkalra@gmail.com>
2026-01-23 10:29:51 -0800

    Modified local_arg to create multiple local files.

--- local_arg/main.tf
+++ local_arg/main.tf
@@ -1,5 +1,11 @@
-resource "local_file" "sample_resource" {
+resource "local_file" "sample_resource1" {
   filename        = "hello_world.txt"
   content         = "Hello World"
+  file_permission = "0644"
+}
+
+resource "local_file" "sample_resource2" {
+  filename        = "bye_world.txt"
+  content         = "Bye World"
   file_permission = "0644"
 }
